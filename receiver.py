8028b8b3d40732eadb8bb10d6bafc721e2a09a33
Karthik <krtdvn@gmail.com>
2023-05-04 11:08:28 +0530

    ready

--- receiver.py
+++ receiver.py
@@ -1,28 +1,46 @@
-import asyncio
 from aiosmtpd.controller import Controller
+import asyncio
+import config
+from email.parser import BytesParser
+import email.policy
+import os
+import telegram
+from telegram.constants import ParseMode
+import uuid
 
-ALLOWED_DOMAINS = ["inmail.1upkd.com", "1upkd.localhost"]
 
-class ExampleHandler:
+parser = BytesParser(policy=email.policy.default)
+os.makedirs("received_mails", exist_ok=True)
+bot = telegram.Bot(token=config.TELEGRAM_BOT_TOKEN)
+
+class MailHandler:
     async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
         domain = address.split("@")[1]
-        if domain not in ALLOWED_DOMAINS:
+        if domain not in config.ALLOWED_DOMAINS:
             return '550 not relaying to that domain'
         envelope.rcpt_tos.append(address)
         return '250 OK'
     
     async def handle_DATA(self, server, session, envelope):
-        print('Message from %s' % envelope.mail_from)
-        print('Message for %s' % envelope.rcpt_tos)
-        print('Message data:\n')
-        for ln in envelope.content.decode('utf8', errors='replace').splitlines():
-            print(f'> {ln}'.strip())
-        print()
-        print('End of message')
+        sender = envelope.mail_from
+        receiver = envelope.rcpt_tos[0]
+        receiver_id = receiver.split("@")[0].split("+")[0][2:]
+        msg_id = str(uuid.uuid1())
+        print(msg_id, sender, receiver)
+        message = parser.parsebytes(envelope.original_content)
+        content = message.get_body("html",).get_content()
+        text = "<b>" + sender + "</b>\n"
+        text += message["Subject"] + "\n\n"
+        url = config.DOMAIN + msg_id + ".html"
+        text += f'<a href="{url}">Read</a>'
+        with open("received_mails/"+msg_id+".html", "w") as f:
+            f.write(content)
+            f.close()
+        await bot.send_message(chat_id=receiver_id, text=text, parse_mode=ParseMode.HTML)
         return '250 Message accepted for delivery'
 
 if __name__=="__main__":
-    controller = Controller(ExampleHandler(), hostname="", port=25)
+    controller = Controller(MailHandler(), hostname="", port=25, decode_data=True)
     controller.start()
     print("Server Listening On", controller.hostname, controller.port)
     a = input("PRESS ENTER TO STOP\n")
--- test_send.py
+++ test_send.py
@@ -1,10 +1,37 @@
+import email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import config
 from smtplib import SMTP as Client
-client = Client("p1.1upkd.com", 25)
-r = client.sendmail('a@1upkd.com', ['b@inmail.1upkd.com'], """\
-From: Anne Person <anne@example.com>
-To: Bart Person <bart@example.com>
-Subject: A test
-Message-ID: <ant>
 
-Hi Bart, this is Anne.
-""")
+sender_email = "a@1upkd.com"
+receiver_email = config.TEST_RECEIVER_ID
+
+
+msg = MIMEMultipart("alternative")
+msg["Subject"] = "multipart test"
+msg["From"] = sender_email
+msg["To"] = receiver_email
+
+html = """\
+<html>
+  <body>
+    <p><b>Python Mail Test</b>
+    <br>
+       This is HTML email with attachment.<br>
+       Click on <a href="https://fedingo.com">Fedingo Resources</a> 
+       for more python articles.
+    </p>
+  </body>
+</html>
+"""
+
+part = MIMEText(html, "html")
+msg.attach(part)
+
+#encoders.encode_base64(part)
+
+client = Client("localhost", 25)
+r = client.sendmail(sender_email, receiver_email, msg.as_string())
