d1e4b800518042d53e56602655a48bb1ff2f36f9
Karthik <krtdvn@gmail.com>
2023-05-04 11:13:06 +0530

    ready

--- receiver.py
+++ receiver.py
@@ -22,21 +22,24 @@ class MailHandler:
         return '250 OK'
     
     async def handle_DATA(self, server, session, envelope):
-        sender = envelope.mail_from
-        receiver = envelope.rcpt_tos[0]
-        receiver_id = receiver.split("@")[0].split("+")[0][2:]
-        msg_id = str(uuid.uuid1())
-        print(msg_id, sender, receiver)
-        message = parser.parsebytes(envelope.original_content)
-        content = message.get_body("html",).get_content()
-        text = "<b>" + sender + "</b>\n"
-        text += message["Subject"] + "\n\n"
-        url = config.DOMAIN + msg_id + ".html"
-        text += f'<a href="{url}">Read</a>'
-        with open("received_mails/"+msg_id+".html", "w") as f:
-            f.write(content)
-            f.close()
-        await bot.send_message(chat_id=receiver_id, text=text, parse_mode=ParseMode.HTML)
+        try:
+            sender = envelope.mail_from
+            receiver = envelope.rcpt_tos[0]
+            receiver_id = receiver.split("@")[0].split("+")[0][2:]
+            msg_id = str(uuid.uuid1())
+            print(msg_id, sender, receiver)
+            message = parser.parsebytes(envelope.original_content)
+            content = message.get_body("html",).get_content()
+            text = "<b>" + sender + "</b>\n"
+            text += message["Subject"] + "\n\n"
+            url = config.DOMAIN + msg_id + ".html"
+            text += f'<a href="{url}">Read</a>'
+            with open("received_mails/"+msg_id+".html", "w") as f:
+                f.write(content)
+                f.close()
+            await bot.send_message(chat_id=receiver_id, text=text, parse_mode=ParseMode.HTML)
+        except:
+            pass
         return '250 Message accepted for delivery'
 
 if __name__=="__main__":
